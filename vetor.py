c3658307306d2645caf009c1ebd1bb9115fbd95e
CAIO MARTINS METELLO <caio.metello@aluno.senac.rio>
2025-06-17 19:05:55 -0300

    oi

---
+++ vetor.py
@@ -0,0 +1,17 @@
+nome = 'luis'
+print(nome)
+
+nome2 = 'renato'
+print(nome2)
+
+#criando um vetor
+vetor = [] #Iniciando vazio
+#com valores pré-cadastrados
+frutas = ['banana', 'maca', 'pera']
+
+#adicionando valores
+vetor.append('Paulo')
+frutas.append("mamão")
+
+print(vetor)
+print(frutas)
